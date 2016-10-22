69a18ae4924e3bf306f02768dd2fe385d1e606ef
Krum Savov <krum.savov@gmail.com>
2016-10-22 16:47:43 +0300

    Homework
    Homework1

---
+++ IntroProgramming/ConsoleApplication3/yAge3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+class Age
+{
+    static void Main()
+    {
+
+        DateTime today = DateTime.Today;
+        string bDay = Console.ReadLine();
+        DateTime birthDate = DateTime.ParseExact(bDay, "MM.dd.yyyy", CultureInfo.InvariantCulture);
+        int age = today.Year - birthDate.Year;
+
+        if (today.Month < birthDate.Month || (today.Month == birthDate.Month && today.Day < birthDate.Day))
+        {
+            age -= ;
+        }
+
+        Console.WriteLine(age);
+        Console.WriteLine(age + 10);
+    }
+}
